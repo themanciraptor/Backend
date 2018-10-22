65ba961ea48eaa6aba0d2d1ac739ae803eab07c1
Jonahw21 <jjwrubleski21@gmail.com>
2018-10-22 12:29:26 -0600

    rename database

--- SASMA.sql
+++ SASMA.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `group_7` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `group_7`;
+CREATE DATABASE  IF NOT EXISTS `SASMA` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `SASMA`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: group_7
+-- Host: localhost    Database: SASMA
 -- ------------------------------------------------------
 -- Server version	8.0.12
 
