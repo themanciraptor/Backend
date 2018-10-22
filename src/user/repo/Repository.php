bde834107dcd839c5b987503ac0688811b38ad2f
Ezra Carter <eah7870ezra@hotmail.com>
2018-10-22 01:40:47 -0600

    start testing!

--- src/user/repo/Repository.php
+++ src/user/repo/Repository.php
@@ -2,25 +2,27 @@
 /**
  * User Repository is for basic data update or access to user table.
  */
-include "src/sql/user/model/User.php";
-use src\util\sql\BaseSQLInterface;
+use src\user\model\User;
+use src\util\sql\SqlInterface;
 
 class UserRepository
 {
     private static $db;
 
-    function __construct(BaseSQLInterface $db)
+    function __construct(SqlInterface $db)
     {
         $this->db = $db;
     }
 
-    public function getUser(string $id)
+    public function getUser(string $id): User
     {
         $getUserQuery = "SELECT * FROM User WHERE user_id = ?";
         $ite = $db->accessorQuery($getUserQuery, "s", $id);
 
         $user = new User;
         $ite->scan(...$user->toRefList());
+
+        return $user;
     }
 }
 
--- tests/user/repo/RepositoryTest.php
+++ tests/user/repo/RepositoryTest.php
@@ -1,19 +1,47 @@
 <?php
-require "src/util/sql/BaseSQL.php";
+require_once "src/util/sql/BaseSQL.php";
+use src\user\model\User;
+require_once "src/user/repo/Repository.php";
 use PHPUnit\Framework\TestCase;
+use phpDocumentor\Reflection\Types\Null_;
 
 class UserRepositoryTest extends TestCase
 {
     protected static $dbh;
+    protected static $repo;
 
     public static function setUpBeforeClass()
     {
         self::$dbh = new Sql("SASMA_test");
+        self::$repo = new UserRepository(self::$dbh);
     }
 
     public static function tearDownAfterClass()
     {
         self::$dbh = null;
+        self::$repo = null;
+    }
+
+    function test_GetUserReturnsUser()
+    {
+        $user = self::$repo->getUser("st-000001");
+
+        $this->assertEquals(self::getExistingUser(), $user);
+    }
+
+    private static function getExistingUser(): User
+    {
+        $values = array(
+            "first_name" => "Carl",
+            "last_name" => "Marks",
+            "user_id" => "st-000001",
+            "email" => "redsocs@rus.com",
+            "created" => "2018-10-18 04:05:58",
+            "updated" => "2018-10-18 04:05:58",
+            "deleted" => Null
+        );
+
+        return User($values);
     }
 }
 
