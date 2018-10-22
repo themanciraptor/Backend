32f247604bd98f13152f7b4949048ad6e6a0390d
Jonahw21 <jjwrubleski21@gmail.com>
2018-10-22 12:24:45 -0600

    undo setup changes

--- db_setup.sql
+++ db_setup.sql
@@ -3,5 +3,5 @@ CREATE USER 'git'@'localhost' IDENTIFIED BY 'gitcommitpassward';
 GRANT ALL PRIVILEGES ON *.* TO 'git'@'localhost' WITH GRANT OPTION;
 
 -- Setup admin user - For MySQL Workbench
-CREATE USER 'jonahw'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'jonahw'@'localhost' WITH GRANT OPTION;
+CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' WITH GRANT OPTION;
