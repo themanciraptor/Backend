44cf0c4d7bd634e65bb3cb0cfb65d3c1b6c49d90
Ezra Carter <eah7870ezra@hotmail.com>
2018-11-14 14:33:53 -0600

    fix test - hadn't updated test after reordering properties

--- tests/user/model/ModelTest.php
+++ tests/user/model/ModelTest.php
@@ -15,11 +15,11 @@ class UserModelTest extends TestCase
 
         // If we change the value, value on object should change
         $refs[0] = $testvar;
-        self::assertEquals($testvar, $usr->first_name);
+        self::assertEquals($testvar, $usr->user_id);
         
         // If we change the reference, the array should change, but the value on the object should not
         $refs[1] = &$testvar;
-        self::assertNotEquals($testvar, $usr->last_name);
+        self::assertNotEquals($testvar, $usr->first_name);
     }
 }
 ?>
