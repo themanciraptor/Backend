715c25f4d3ae61c500ca8f9252a5b838304ad1a2
Jonahw21 <jjwrubleski21@gmail.com>
2018-11-16 13:37:31 -0600

    Drop unnecessary tables

--- SASMA.sql
+++ SASMA.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `SASMA` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `SASMA`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: SASMA
+-- Host: localhost    Database: sasma
 -- ------------------------------------------------------
 -- Server version	8.0.12
 
@@ -18,33 +16,6 @@ USE `SASMA`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `auth`
---
-
-DROP TABLE IF EXISTS `auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `auth` (
-  `is_admin` tinyint(4) DEFAULT NULL,
-  `password` varchar(45) NOT NULL,
-  `user_id` varchar(45) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `auth`
---
-
-LOCK TABLES `auth` WRITE;
-/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
-INSERT INTO `auth` VALUES (0,'password','uid1', '2018-10-18 10:05:58','2018-10-18 10:05:58');
-/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `college`
 --
 
@@ -52,11 +23,12 @@ DROP TABLE IF EXISTS `college`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `college` (
+  `id` varchar(20) NOT NULL,
   `name` varchar(200) NOT NULL,
   `address` varchar(200) NOT NULL,
-  `id` varchar(20) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -67,60 +39,9 @@ CREATE TABLE `college` (
 
 LOCK TABLES `college` WRITE;
 /*!40000 ALTER TABLE `college` DISABLE KEYS */;
-INSERT INTO `college` VALUES ('University of Regina','3737 Wascana Parkway','UofR', '2018-10-18 10:05:58','2018-10-18 10:05:58');
+INSERT INTO `college` VALUES ('UofR','University of Regina','3737 Wascana Parkway','2018-10-18 10:05:58','2018-10-18 10:05:58',NULL);
 /*!40000 ALTER TABLE `college` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `programs`
---
-
-DROP TABLE IF EXISTS `program`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `program` (
-  `name` varchar(60) NOT NULL,
-  `id` varchar(20) NOT NULL,
-  `capacity` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `program`
---
-
-LOCK TABLES `program` WRITE;
-/*!40000 ALTER TABLE `program` DISABLE KEYS */;
-INSERT INTO `program` VALUES ('Things you already know','CS 372',50,'2018-10-18 10:05:58','2018-10-18 10:05:58');
-/*!40000 ALTER TABLE `program` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `program_student`
---
-
-DROP TABLE IF EXISTS `program_student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `program_student` (
-  `program_id` varchar(30) NOT NULL,
-  `student_id` varchar(30) NOT NULL,
-  PRIMARY KEY (`program_id`,`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `program_student`
---
-
-LOCK TABLES `program_student` WRITE;
-/*!40000 ALTER TABLE `program_student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `program_student` ENABLE KEYS */;
-UNLOCK TABLES;
-
 
 --
 -- Table structure for table `student`
@@ -131,14 +52,17 @@ DROP TABLE IF EXISTS `student`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `student` (
   `id` varchar(20) NOT NULL,
+  `user_id` varchar(30) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
   `address` varchar(200) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `deleted` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_idx` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -148,7 +72,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('200361084','Jonah','Wrubleski','jjwrubleski21@gmail.com','3062318046','Ma house', '2018-10-18 10:05:58','2018-10-18 10:05:58');
+INSERT INTO `student` VALUES ('200361084','','Jonah','Wrubleski','jjwrubleski21@gmail.com','Ma house','3062318046','2018-10-18 10:05:58','2018-10-18 10:05:58',NULL);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,13 +85,16 @@ DROP TABLE IF EXISTS `student_term_data`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `student_term_data` (
   `id` varchar(45) NOT NULL,
+  `college_id` varchar(45) NOT NULL,
+  `student_id` varchar(45) NOT NULL,
   `enrollment_status` varchar(45) NOT NULL,
   `term` varchar(45) NOT NULL,
-  `college_id` varchar(45) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `college_id2_idx` (`college_id`),
+  KEY `id_idx` (`student_id`),
   CONSTRAINT `id` FOREIGN KEY (`college_id`) REFERENCES `college` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -178,59 +105,35 @@ CREATE TABLE `student_term_data` (
 
 LOCK TABLES `student_term_data` WRITE;
 /*!40000 ALTER TABLE `student_term_data` DISABLE KEYS */;
-INSERT INTO `student_term_data` VALUES ('student_id_200361084','Enrolled','Fall/2018','UofR', '2018-10-18 10:05:58','2018-10-18 10:05:58');
+INSERT INTO `student_term_data` VALUES ('student_id_200361084','UofR','','Enrolled','Fall/2018','2018-10-18 10:05:58','2018-10-18 10:05:58',NULL);
 /*!40000 ALTER TABLE `student_term_data` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `term_data_courses`
---
-
-DROP TABLE IF EXISTS `term_data_courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `term_data_courses` (
-  `term_data_id` varchar(30) NOT NULL,
-  `program_id` varchar(30) NOT NULL,
-  PRIMARY KEY (`term_data_id`,`program_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `term_data_courses`
---
-
-LOCK TABLES `term_data_courses` WRITE;
-/*!40000 ALTER TABLE `term_data_courses` DISABLE KEYS */;
-/*!40000 ALTER TABLE `term_data_courses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
-  `user_id` varchar(30) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `id` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
+  `deleted` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES ('st-000001','Carl','Marks','redsocs@rus.com','2018-10-18 10:05:58','2018-10-18 10:05:58');
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('st-000001','redsocs@rus.com','2018-10-18 10:05:58','2018-10-18 10:05:58',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -242,4 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-22 12:12:33
+-- Dump completed on 2018-11-16 13:36:05
